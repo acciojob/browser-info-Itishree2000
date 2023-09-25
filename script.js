ac686b2c62d61e2d55e00cec509b707d3352e2be
Itishree2000 <127124225+Itishree2000@users.noreply.github.com>
2023-09-25 17:02:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required
+// Get a reference to the div element with the id "browser-info"
+const browserInfoDiv = document.getElementById("browser-info");
+
+// Get browser name and version from the navigator object
+const browserName = navigator.appName;
+const browserVersion = navigator.appVersion;
+
+// Display the browser information inside the div
+browserInfoDiv.textContent = "You are using " + browserName + " version " + browserVersion;
